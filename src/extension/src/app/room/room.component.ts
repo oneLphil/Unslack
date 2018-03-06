8b2799f29da56fdad295b2bc08024b48f09e96e1
Graeme Stroud <graeme.stroud@mail.utoronto.ca>
2018-03-06 18:50:05 -0500

    Created basic room list

--- src/extension/src/app/room/room.component.ts
+++ src/extension/src/app/room/room.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { Room } from '../room';
+import { ROOMS } from '../mock-rooms';
 
 @Component({
   selector: 'app-room',
   templateUrl: './room.component.html',
   styleUrls: ['./room.component.css']
 })
+
+
 export class RoomComponent implements OnInit {
 
+  rooms = ROOMS;
+
+  selectedRoom: Room;
   constructor() { }
 
+
   ngOnInit() {
+  }
+
+  onSelect(room: Room): void {
+    this.selectedRoom = room;
   }
 
 }
