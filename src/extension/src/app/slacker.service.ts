31a52fb448e2af5ff97a0c66a0498b41dac32b81
pineapplebun <kevinjr.chen1@gmail.com>
2018-03-06 21:05:09 -0500

    comment out future code

--- src/extension/src/app/slacker.service.ts
+++ src/extension/src/app/slacker.service.ts
@@ -1,14 +1,11 @@
 /*
-SlackerServices provides a service to components for slacker information retrieval,
-augmentation and management.
+SlackerServices provides a service to the Slacker components for slacker
+information retrieval, augmentation and management.
 
 functions:
 
-getSlackers(roomId:number): Slacker[]
-getSlacker(userId:number): Slacker
-... (maybe more if we can get statistics from RoomService)
+myUsage : UsageChart
 
-retrieves data of all slackers for a given room id.
 
 */
 
@@ -29,15 +26,11 @@ export class SlackerService {
     private roomService: RoomService
     ) { }
 
-  /* Requests an Array of Slackers from RoomService */
+  /*
+  // Requests an Array of Slackers from RoomService
   getSlackers(roomId: number): Slacker[]> {
     this.messageService.add('SlackerService: fetched slackers');
     return this.RoomService.getLocalRoomData().slackers;
-    /*return this.http.get<Slacker[]>(this.slackersUrl)
-      .pipe(
-        tap(slackers => this.log(`fetched slackers`)),
-        catchError(this.handleError('getHeroes, []'))
-      );*/
   }
 
   getSlacker(id: number): Slacker {
@@ -46,49 +39,14 @@ export class SlackerService {
     // local file version
     return this.RoomService.getLocalRoomData().slackers.find(slacker => slacker.id === id);
 
-    // http version
-    /*
-    const url = `${this.slackersUrl}/${id}`;
-    return this.http.get<Slacker>(url).pipe(
-      tap(_ => this.log(`fetched slacker id=${id}`)),
-      catchError(this.handleError<Slacker>(`getSlacker id=${id}`))
-    ); */
   }
 
-  /** Log a SlackerService message with the MessageService */
+  // Log a SlackerService message with the MessageService
   private log(message: string) {
     this.messageService.add('SlackerService: ' + message);
   }
-
-
-  /** PUT: update the hero on the server
-  updateSlacker (slacker: Slacker): Observable<any> {
-    return this.http.put(this.slackersUrl, slacker, httpOptions).pipe(
-      tap(_ => this.log(`updated slacker id=${slacker.id}`)),
-      catchError(this.handleError<any>('updateSlacker'))
-    );
-  }
   */
 
-  /**
-   * Handle Http operation that failed.
-   * Let the app continue.
-   * @param operation - name of the operation that failed
-   * @param result - optional value to return as the observable result
-
-  private handleError<T> (operation = 'operation', result?: T) {
-    return (error: any): Observable<T> => {
-
-      // TODO: send the error to remote logging infrastructure
-      console.error(error); // log to console instead
-
-      // TODO: better job of transforming error for user consumption
-      this.log(`${operation} failed: ${error.message}`);
-
-      // Let the app keep running by returning an empty result.
-      return of(result as T);
-    };
-  }*/
 
 
 }
