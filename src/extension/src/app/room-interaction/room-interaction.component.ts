3fff952065b203df0f5131c1c51fd5d9b1c85e9b
pineapplebun <kevinjr.chen1@gmail.com>
2018-03-09 00:11:43 -0500

    fix table component error and add interaction box

---
+++ src/extension/src/app/room-interaction/room-interaction.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-room-interaction',
+  templateUrl: './room-interaction.component.html',
+  styleUrls: ['./room-interaction.component.css']
+})
+export class RoomInteractionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
